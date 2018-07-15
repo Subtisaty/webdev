ecbd64de081fe48d6a766a45c9ab41d1e5813cbc
Jonathon <subtisaty@null.net>
2018-07-15 17:23:46 +0100

    HTML/CSS

---
+++ Fully Developed Webpages/Project - 001 [Unnamed]/templates/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<link rel = "stylesheet" type = "text/css" href = "/styles/main.stylesheet">
+<link rel = "stylesheet" type = "text/css" href = "/styles/header.stylesheet">
+<link rel = "stylesheet" type = "text/css" href = "/styles/nav.stylesheet">
+<link rel = "stylesheet" type = "text/css" href = "/styles/footer.stylesheet">
+
+<title>Unamed Project</title>    
+<desc>This projet has no available description.</desc>    
+</head>
+<body>
